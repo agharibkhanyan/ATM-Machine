84dcdb3a1db255f66abd2fab7e8e37150943d80d
artur <InFeSt@arturs-MacBook-Pro.local>
2017-12-22 15:46:06 +0400

    this is my java atm

---
+++ javaproject/Transaction.java
@@ -0,0 +1,30 @@
+package assignment1;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.Serializable;
+public class Transaction implements Serializable {
+	private int transNumber;
+	private int transId;
+    private double transAmt;
+    public Transaction(int number, int id, double amount){
+    	 transNumber = number;
+         transId = id;
+         transAmt = amount;
+         
+    }
+ 
+    public int getTransNumber() {
+		return transNumber;
+	}
+	
+	public int getTransId() {
+		return transId;
+	}
+
+	public double getTransAmt() {
+		return transAmt;
+	}
+	
+}
+
